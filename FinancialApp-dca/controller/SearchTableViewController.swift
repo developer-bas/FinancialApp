e90cfd77f7fed3f6950b8b0b80a5d8e94769235a
Sebastian <cruzguzmaneric@gmail.com>
2021-01-08 02:34:36 -0600

    Handle search results & displaying results on tableviewcell

--- FinancialApp-dca/controller/SearchTableViewController.swift
+++ FinancialApp-dca/controller/SearchTableViewController.swift
@@ -23,38 +23,63 @@ class SearchTableViewController: UITableViewController {
     
     private let apiService = APIService()
     private var subscribers = Set<AnyCancellable>()
+    private var searchResults : SearchResults?
+    @Published private var searchQuery = String()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupNavigationBar()
-        performSearch()
+        observForm()
+//        performSearch()
+    }
+    private func observForm(){
+        $searchQuery.debounce(for: .milliseconds(750), scheduler: RunLoop.main)
+            .sink { [unowned self](searchQuery) in
+                
+                self.apiService.fetchSymbolsPublisher(keywords: searchQuery).sink { (completion) in
+                    switch completion{
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                    case .finished : break
+                    }
+                } receiveValue: { (searchResults) in
+                    self.searchResults = searchResults
+                    self.tableView.reloadData()
+                }.store(in: &self.subscribers)
+            }.store(in: &subscribers)
     }
     
-    func performSearch(){
-        apiService.fetchSymbolsPublisher(keywords: "S&P500").sink { (completion) in
-            switch completion{
-            case .failure(let error):
-                print(error.localizedDescription)
-            case .finished : break
-            }
-        } receiveValue: { (searchResults) in
-            print("\(searchResults)")
-        }.store(in: &subscribers)
-
-    }
+//    func performSearch(){
+//        apiService.fetchSymbolsPublisher(keywords: "S&P500").sink { (completion) in
+//            switch completion{
+//            case .failure(let error):
+//                print(error.localizedDescription)
+//            case .finished : break
+//            }
+//        } receiveValue: { (searchResults) in
+//            print("\(searchResults)")
+//        }.store(in: &subscribers)
+//
+//    }
     
     func setupNavigationBar(){
         navigationItem.searchController = searchController
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return searchResults?.items.count ?? 0
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
          
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as! SearchTableViewCell
+        
+        if let searchResults = self.searchResults{
+            let searchResult = searchResults.items[indexPath.row]
+            cell.configure(with: searchResult)
+        }
+        
         
         return cell
     }
@@ -64,8 +89,13 @@ class SearchTableViewController: UITableViewController {
 }
 
 extension SearchTableViewController : UISearchResultsUpdating, UISearchControllerDelegate {
+    
     func updateSearchResults(for searchController: UISearchController) {
-        
+        guard let searchQuery = searchController.searchBar.text , !searchQuery.isEmpty else {return}
+    
+        self.searchQuery = searchQuery
+//        print(searchQuery)
+    
     }
     
     
