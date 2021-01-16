4b9bf230555af9e57d0fd32f8b4a3e8f01503552
Sebastian <cruzguzmaneric@gmail.com>
2021-01-15 23:42:23 -0600

    Configuring UI with Asset

--- FinancialApp-dca/controller/CalculatorTableViewController.swift
+++ FinancialApp-dca/controller/CalculatorTableViewController.swift
@@ -13,7 +13,16 @@ class CalculatorTableViewController : UITableViewController{
     @IBOutlet weak var symbolLabel: UILabel!
     @IBOutlet weak var assetNameLabel : UILabel!
     
+    var asset: Asset?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpViews()
     }
+    
+    private func setUpViews(){
+        symbolLabel.text = asset?.searchResult.symbol
+        assetNameLabel.text = asset?.searchResult.name
+    }
+    
 }
