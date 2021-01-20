a732d58d6188e65ba7e10afe1193276ed7e4c542
Sebastian <cruzguzmaneric@gmail.com>
2021-01-19 23:57:47 -0600

    Index out of range bug fix

--- FinancialApp-dca/controller/CalculatorTableViewController.swift
+++ FinancialApp-dca/controller/CalculatorTableViewController.swift
@@ -52,7 +52,8 @@ class CalculatorTableViewController : UITableViewController{
     
     private func setupDateSlider(){
         if let count = asset?.timeSeriesMonthlyAdjusted.getMonthInfo().count{
-            dateSlider.maximumValue = count.floatValue
+            let dateSliderCount = count - 1
+            dateSlider.maximumValue = dateSliderCount.floatValue
         }
     }
     
@@ -108,9 +109,9 @@ extension CalculatorTableViewController: UITextFieldDelegate{
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
         if textField == initialDateOfInvestmentTextField{
             performSegue(withIdentifier: "showDateSelection", sender: asset?.timeSeriesMonthlyAdjusted)
-            
+            return false
         }
-        return false
+        return true 
     }
     
 }
