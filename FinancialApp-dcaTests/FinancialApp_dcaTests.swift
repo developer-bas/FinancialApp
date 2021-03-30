37fae5c623d6b8cf1e40c14fd3a5fcace61712a5
Eric Sebastian <cruzguzmaneric@gmail.com>
2021-03-29 23:25:03 -0600

    Complete first unit test

--- FinancialApp-dcaTests/FinancialApp_dcaTests.swift
+++ FinancialApp-dcaTests/FinancialApp_dcaTests.swift
@@ -41,11 +41,20 @@ class FinancialApp_dcaTests: XCTestCase {
         
         let initialDateOfInvestmentIndex = 5
         
+        let asset = buildWinningAsset()
         //when
         
-      //  let result = sut.calculate(asset: <#T##Asset#>, initialInvestmentAmount: <#T##Double#>, monthlyDollarCostAveragingAmount: <#T##Double#>, initialDateOfInvestmentIndex: <#T##Int#>)
+        let result = sut.calculate(asset: asset, initialInvestmentAmount: initialInvestmentAmount, monthlyDollarCostAveragingAmount: monthlyDollarCostAveragingAmount, initialDateOfInvestmentIndex: initialDateOfInvestmentIndex)
         
         //then
+        
+        
+        XCTAssertEqual(result.investementAmount, 12500, "Investment amount is incorrect")
+        XCTAssertTrue(result.isProfitable)
+        XCTAssertEqual(result.currencyValue, 17342.224 , accuracy: 0.1 )
+        XCTAssertEqual(result.gain, 4842.224, accuracy: 01)
+        XCTAssertEqual(result.yield, 0.3873, accuracy: 0.0001)
+        
     }
     
     func testResult_givenWinningAssetAndDCAIsNotUsed_expectPositiveGains(){
@@ -62,11 +71,29 @@ class FinancialApp_dcaTests: XCTestCase {
     
     private func buildWinningAsset() -> Asset {
        
-    //    return Asset(searchResult: <#T##SearchResult#>, timeSeriesMonthlyAdjusted: <#T##TimeSeriesMonthlyAdjusted#>)
+        let searchResult = buildSearchResults()
+        let meta = buildMeta()
+        let timeSeries : [String : OHLC] =
+            ["2021-01-25": OHLC(open: "100", close: "110", adjustedClose: "110"),
+             "2021-02-25": OHLC(open: "110", close: "120", adjustedClose: "120"),
+             "2021-03-25": OHLC(open: "120", close: "130", adjustedClose: "130"),
+             "2021-04-25": OHLC(open: "130", close: "140", adjustedClose: "140"),
+             "2021-05-25": OHLC(open: "140", close: "150", adjustedClose: "150"),
+             "2021-06-25": OHLC(open: "150", close: "160", adjustedClose: "160")
+            ]
+        
+        
+        let timeSriesMonthltyAdjusted = TimeSeriesMonthlyAdjusted(meta: meta, timeSeries: timeSeries)
+        
+        return Asset(searchResult: searchResult, timeSeriesMonthlyAdjusted: timeSriesMonthltyAdjusted)
     }
     
-    private func buildSearchResul() -> SearchResult {
+    private func buildSearchResults() -> SearchResult {
         return SearchResult(symbol: "XYZ", name: "XYZ Company", type: "ETF", currency: "USD")
+    }
+    
+    private func buildMeta() -> Meta {
+        return Meta(symbol: "XYZ")
     }
     
     func testInvestmentAmount_whenDCAIsUsed_expectResult(){
